fixed bras, but there is still a issue with gradient

BLOCK_SPG_CPD.py
@@ -12,17 +12,24 @@ import numpy as np
 Computes brasCPD for 3 dimensional tensors.
 Returns A,B,C
 """
-def bras_CPD(A_true, X, rank, B, alpha, beta, num_iterations=100):
-	# Keep residual errors
-	cost = []
+def bras_CPD(F, X, rank, B, alpha, beta, num_iterations=100):
+	# bookkeeping
+	res_error = []
 	mse = []
+	norm_x = norm(X)
+	F_norm = [norm(F[0]), norm(F[1]), norm(F[2])]
 
 	# Randomly initialize A,B,C
 	dim_1, dim_2, dim_3 = X.shape
 	A = [rand_init(dim_1, rank), rand_init(dim_2, rank), rand_init(dim_3, rank)]
 	total_col = {0:dim_2*dim_3, 1:dim_1*dim_3, 2:dim_1*dim_2}
-	cost.append(residual_error(X, A))
-	mse.append(MSE(A_true, A))
+
+
+	# Append initialization residual error
+	res_error.append(residual_error(X, norm_x, A))
+	mse.append(MSE(F, F_norm, A))
+	# cost.append(residual_error(X, A))
+
 	# Run bras_CPD
 	for r in range(num_iterations):
 		if (r+1)%1000 == 0:
@@ -40,26 +47,35 @@ def bras_CPD(A_true, X, rank, B, alpha, beta, num_iterations=100):
 		alpha /= (r+1)**beta
 
 		# Append error
-		cost.append(residual_error(X, A))
-		mse.append(MSE(A_true, A))
-	return A, np.array(cost), np.array(mse)
+		res_error.append(residual_error(X, norm_x, A))
+		mse.append(MSE(F, F_norm, A))
+
+
+	return res_error, mse
 
 """
 Computes AdaCPD for 3 dimensional tensors.
 Returns A,B,C
 """
-def ada_CPD(A_true, X, rank, B, eta, b, eps, num_iterations=100):
-	# Keep residual errors
-	cost = []
+def ada_CPD(F, X, rank, B, eta, b, eps, num_iterations):
+	# bookkeeping
+	res_error = []
 	mse = []
+	norm_x = norm(X)
+	F_norm = [norm(F[0]), norm(F[1]), norm(F[2])]
 
 	# Randomly initialize A,B,C
 	dim_1, dim_2, dim_3 = X.shape
 	A = [rand_init(dim_1, rank), rand_init(dim_2, rank), rand_init(dim_3, rank)]
-	G = [np.zeros((dim_1, rank), dtype=float)+ 1,np.zeros((dim_2, rank), dtype=float)+ 1,np.zeros((dim_3, rank), dtype=float)+ 1]
+	G = [np.zeros((dim_1, rank), dtype=float),np.zeros((dim_2, rank), dtype=float),np.zeros((dim_3, rank), dtype=float)]
 	total_col = {0:dim_2*dim_3, 1:dim_1*dim_3, 2:dim_1*dim_2}
-	cost.append(residual_error(X, A))
-	mse.append(MSE(A_true, A))
+
+
+	# Append initialization residual error
+	res_error.append(residual_error(X, norm_x, A))
+	mse.append(MSE(F, F_norm, A))
+	# cost.append(residual_error(X, A))
+	A[0] = np.clip(A[0], a_min=0,a_max=0.1)
 
 	# Run bras_CPD
 	for r in range(num_iterations):
@@ -75,6 +91,7 @@ def ada_CPD(A_true, X, rank, B, eta, b, eps, num_iterations=100):
 		update_factor_ada(X, A, G, idx, n, rank, eta, b, eps)
 
 		# Append error
-		cost.append(residual_error(X, A))
-		mse.append(MSE(A_true, A))
-	return A, np.array(cost), np.array(mse)
+		res_error.append(residual_error(X, norm_x, A))
+		mse.append(MSE(F, F_norm, A))
+
+	return res_error, mse

helper.py
@@ -29,7 +29,7 @@ def unfold(tensor, mode=0):
 Generates sketching indices
 """
 def generate_sketch_indices(B, total_col):
-	return np.random.choice(range(total_col), size=B, replace=False, p=None)
+	return np.random.choice(total_col, size=B, replace=False, p=None)
 
 """
 Update a single factor matrix via gradient update
@@ -39,6 +39,8 @@ def update_factor_bras(X, A, idx, n, rank, alpha):
 	H_n = (tl_alg.khatri_rao(A, skip_matrix=n))[idx, :]
 	A[n] -= alpha/rank * (A[n] @ H_n.T @ H_n - X_n.T @ H_n)
 
+	# Apply proximal regularization constraint (A > 0)
+	A[n] = np.clip(A[n], a_min=0, a_max=None)
 
 """
 Update a single factor matrix via gradient update
@@ -56,6 +58,9 @@ def update_factor_ada(X, A, G, idx, n, rank, eta, b, eps):
 	# Update factor matrices
 	A[n] -= lr*grad
 
+	# Apply proximal regularization constraint (A > 0)
+	A[n] = np.clip(A[n], a_min=0, a_max=None)
+
 	# Accumulate gradient information.
 	G[n] += grad**(2)
 
@@ -69,22 +74,20 @@ def compute_learning_rate(G_n, b, eta, eps):
 """
 Computes Normalized MSE between the factor matrices:
 """
-def MSE(A_true, A):
-	F = len(A_true)
+def MSE(F, F_norm, A):
 	mse = 0
-	for i in range(len(A_true)):
-		A_true_norm = A_true[i] / norm(A_true[i])
+	for i in range(len(A)):
+		A_true_norm = F[i] / F_norm[i]
 		A_norm = A[i] / norm(A[i])
 		mse += norm(A_true_norm - A_norm)**2
-	return mse/F
+	return mse/len(A)
 
 """
 Computes residual error
 """
-def residual_error(X, A):
-	dim_1, dim_2, dim_3 = X.shape
+def residual_error(X, norm_x, A):
 	X_bar = tl_kruskal.kruskal_to_tensor(A)
-	return norm(X-X_bar)/(dim_1*dim_2*dim_3)
+	return norm(X-X_bar)/norm_x
 
 """
 Computes norm of a tensor